ed6abe37ac64fca96efa4c5d5df99840021a94f6
rmdkak <rmdkak99786@naver.com>
2023-07-19 11:15:12 +0900

    feat: map 검색창 및 검색 리스트
    map 검색창 구현 검색 시 사이드바 위치에 모달로 검색한 페이지 리스트 생성
    리스트 클릭 시 지도 상 마커에 info 데이터 확인 가능 및 해당 마커
    중심으로 지도 이동 구현

--- src/components/common/Modal/Modal.styled.js
+++ src/components/common/Modal/Modal.styled.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Outer = styled.div`
   position: fixed;
@@ -8,21 +8,49 @@ export const Outer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
+  z-index: 999;
   width: 100%;
   height: 100%;
 
-  background-color: rgba(0, 0, 0, 0.3);
+  ${({ type }) => {
+    if (type === "main") {
+      return css`
+        background-color: transparent;
+        pointer-events: none;
+      `;
+    } else {
+      return css`
+        background-color: rgba(0, 0, 0, 0.3);
+      `;
+    }
+  }}
 `;
 
 export const Inner = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  padding: 50px;
-
-  // FIXME white 고정?
-  background-color: ${props => props.theme.colors[props.$bgcolor]};
-  /* border-radius: 70px; */
+  ${({ type }) => {
+    if (type === "main") {
+      return css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 25px;
+        background-color: rgb(31, 31, 34, 1);
+        margin-right: auto;
+        width: 14vw;
+        min-width: 200px;
+        height: 100%;
+        pointer-events: auto;
+      `;
+    } else {
+      return css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 50px;
+        // FIXME white 고정?
+        background-color: ${props => props.theme.colors[props.$bgcolor]};
+        /* border-radius: 70px; */
+      `;
+    }
+  }}
 `;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,19 +1,180 @@
 import styled from "styled-components";
-import { Input } from "components/common";
+import { Input, Modal } from "components/common";
 import Sidebar from "components/Sidebar/Sidebar";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal, openModal } from "redux/modules/modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import logo from "assets/logo.png";
+
+const { kakao } = window;
 
 export const Main = () => {
+  const { ListIsOpen } = useSelector(state => state.modal);
+  const dispatch = useDispatch();
+  const modalOpenHandler = target => dispatch(openModal(target));
+  const [test, setTest] = useState("");
+  const [info, setInfo] = useState();
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState();
+  const [dataList, setDataList] = useState([]);
+  const [pagination, setPagination] = useState(null);
+  const [position, setPosition] = useState({
+    center: {
+      lat: 37.566826,
+      lng: 126.9786567
+    },
+    isPanto: false
+  });
+
+  const testSubmit = e => {
+    e.preventDefault();
+    const ps = new kakao.maps.services.Places();
+
+    if (!test.replace(/^\s+|\s+$/g, "")) {
+      alert("키워드를 입력해주세요!");
+      return;
+    }
+
+    ps.keywordSearch(test, (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setPagination(_pagination);
+        const bounds = new kakao.maps.LatLngBounds();
+        let markers = [];
+
+        for (let i = 0; i < data.length; i++) {
+          setDataList(data);
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x
+            },
+            content: data[i].place_name
+          });
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        setMarkers(markers);
+
+        map.setBounds(bounds);
+        modalOpenHandler("ListIsOpen");
+      }
+    });
+  };
+
+  const prevPage = () => {
+    if (pagination === null) return;
+    if (pagination.hasPrevPage) pagination.prevPage();
+  };
+  const NextPage = () => {
+    if (pagination === null) return;
+    if (pagination.hasNextPage) pagination.nextPage();
+  };
+
+  const modalCloseHandler = () => {
+    dispatch(closeModal("ListIsOpen"));
+    setTest("");
+    setMarkers([]);
+    setDataList([]);
+    setPosition({
+      center: {
+        lat: 37.566826,
+        lng: 126.9786567
+      },
+      isPanto: false
+    });
+  };
+
+  const showInfoHandler = data => {
+    setInfo({ content: data.place_name });
+    setPosition({
+      center: { lat: data.y, lng: data.x },
+      isPanto: true
+    });
+  };
+
   // TODO 반응형
   return (
     <Container>
+      {ListIsOpen && (
+        <Modal type={"main"}>
+          <Modaldiv>
+            <ImgBox>
+              <Img src={logo} alt={"plan-it-travel"} />
+              <XButton onClick={modalCloseHandler}>
+                <FontAwesomeIcon icon={faXmark} size="2xl" style={{ color: "#ffffff" }} />
+              </XButton>
+            </ImgBox>
+            <ModalUl>
+              검색 결과:{pagination?.totalCount}건
+              {dataList?.map(e => (
+                <ModalLi key={e.id} onClick={() => showInfoHandler(e)}>
+                  {e.place_name}
+                </ModalLi>
+              ))}
+            </ModalUl>
+            <MoveBtnBox>
+              <button onClick={prevPage}>
+                <FontAwesomeIcon icon={faChevronLeft} size="lg" style={{ color: "#ffffff" }} />
+              </button>
+              <span>
+                {pagination?.current}...
+                {pagination?.last}
+              </span>
+              <button onClick={NextPage}>
+                <FontAwesomeIcon icon={faChevronRight} size="lg" style={{ color: "#ffffff" }} />
+              </button>
+            </MoveBtnBox>
+          </Modaldiv>
+        </Modal>
+      )}
       <Sidebar />
       <MapContainer>
-        <Input size={"large"} type="search" placeholder="Search" />
-        <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100%" }}>
-          {/* <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}> */}
-          {/* <div style={{ color: "#000" }}>Hello World!</div> */}
-          {/* </MapMarker> */}
+        <form onSubmit={testSubmit}>
+          <Input
+            size={"large"}
+            $bgcolor={"black"}
+            type="search"
+            placeholder="Search"
+            value={test}
+            onChange={e => setTest(e.target.value)}
+          />
+        </form>
+        {/* <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "100%", height: "100%" }}>
+          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Hello World!</div>
+          </MapMarker>
+        </Map> */}
+        <Map // 로드뷰를 표시할 Container
+          center={position.center}
+          isPanto={position.isPanto}
+          style={{
+            width: "100%",
+            height: "100%",
+            zIndex: "1"
+          }}
+          level={3}
+          onCreate={setMap}
+        >
+          {markers.map(marker => (
+            <MapMarker
+              key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+              position={marker.position}
+              onClick={() => setInfo(marker)}
+            >
+              {info && info.content === marker.content && (
+                <CustomOverlayMap
+                  position={marker.position}
+                  xAnchor={0.5}
+                  yAnchor={1.45}
+                  zIndex={3}
+                >
+                  <OverlayDiv>{marker.content}</OverlayDiv>
+                </CustomOverlayMap>
+              )}
+            </MapMarker>
+          ))}
         </Map>
       </MapContainer>
     </Container>
@@ -31,23 +192,78 @@ const MapContainer = styled.div`
   justify-content: center;
   align-items: center;
 
-  & > input {
-    width: 300px;
-    height: 40px;
-    border-radius: 5px;
-    /* width: 438px;
-    height: 56px;
-    border-radius: 16px; */
-    border: none;
-    background-color: #1f1f22;
+  & > form {
     z-index: 999;
     position: fixed;
     top: 20px;
-    outline: none;
-    padding-left: 10px;
-
-    &::placeholder {
-      color: white;
-    }
   }
+`;
+
+const Modaldiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 60px;
+`;
+
+const XButton = styled.button`
+  position: absolute;
+  top: 1%;
+  left: 15%;
+  background-color: #1f1f22;
+  width: 30px;
+  height: 50px;
+  padding-right: 5px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+`;
+
+const Img = styled.img`
+  display: inherit;
+  align-self: center;
+  width: 150px;
+  margin: 20px;
+`;
+
+const ModalUl = styled.ul`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: baseline;
+  gap: 10px;
+`;
+
+const ModalLi = styled.li`
+  padding: 5px 3px;
+  width: 100%;
+  &:hover {
+    cursor: pointer;
+    background-color: #a290e6;
+    border-radius: 5px;
+  }
+`;
+
+const MoveBtnBox = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: auto;
+`;
+
+const OverlayDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 150px;
+  height: 100px;
+  padding: 10px;
+  border: 1px solid #1f1f22;
+  background-color: #1f1f22;
+  border-radius: 5px;
 `;
