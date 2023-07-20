5444e6e41aa3ae203106f88e30f2eeef7ffcbc94
Yujin Kim <cocodj0509@naver.com>
2023-07-20 15:02:26 +0900

    디테일페이지 파일생성

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -5,14 +5,8 @@ const KEY = `${process.env.REACT_APP_YOUTUBE_KEY}`;
 const youtubeApi = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    part: "snippet",
-    maxResults: 5,
-    key: KEY,
-    category: "news"
-    // id: 25
-    // videoCategoryId: 25
-  },
-  headers: {}
+    key: KEY
+  }
 });
 
 export { youtubeApi };
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -9,8 +9,6 @@ import { getUsers } from "api/users";
 import uuid from "react-uuid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
-import { youtubeApi } from "../api/youtube";
-import YouTube from "react-youtube";
 import { faSpinner, faSquareCaretUp, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { throttle } from "lodash";
 
@@ -127,32 +125,14 @@ export const Detail = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  const [youtubeRes, setYoutubeRes] = useState("");
-
-  const onSearch = async () => {
-    console.log("response");
-    // var value = encodeURI("여행계획 목포해수욕장");
-    const response = await youtubeApi.get("/search", {
-      params: {
-        q: "목포"
-      }
-    });
-    setYoutubeRes(response.data.items[0].id.videoId);
-    console.log(response.data.items, "response");
-  };
-  //유튜브
-  useEffect(() => {
-    onSearch();
-  }, []);
-
   return (
     <Container>
       <Wrap>
         <Map // 지도를 표시할 Container
           center={position}
           style={{
-            width: "600px",
-            height: "600px"
+            width: "700px",
+            height: "700px"
           }}
           level={3}
           draggable={draggable}
@@ -160,22 +140,6 @@ export const Detail = () => {
         >
           <MapMarker position={position} />
         </Map>
-
-        <YouTube
-          videoId={youtubeRes}
-          opts={{
-            width: "700",
-            height: "400",
-            playerVars: {
-              autoplay: 1,
-              rel: 0,
-              modestbranding: 1
-            }
-          }}
-          onEnd={e => {
-            e.target.stopVideo(0);
-          }}
-        ></YouTube>
       </Wrap>
       <CommentsLeaveWrap>
         <form onSubmit={leaveCommentHandler}>
@@ -275,9 +239,8 @@ const Container = styled.div`
 `;
 const Wrap = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   padding-top: 60px;
-  align-items: center;
 `;
 const Flex = styled.div`
   display: flex;
